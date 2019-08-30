94a2bb8e655c1eed16dc105df45a39cf94279253
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-30 00:52:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,6 +39,10 @@ def hit?
   # code hit? here
   prompt_user
   user_input = get_user_input
+  if user_input == "h"
+    deal_card
+  elsif user_input != "s"
+    invalid_command
 end
 
 def invalid_command
